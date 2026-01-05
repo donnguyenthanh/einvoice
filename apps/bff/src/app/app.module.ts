3548c7633d7f5db190023a69e2ade70a6f3b0ed3
dondonkongu <obitanz123@gmail.com>
2026-01-05 10:33:26 +0700

    chore(config): init lib middleware

--- apps/bff/src/app/app.module.ts
+++ apps/bff/src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {ConfigModule} from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
 import { CONFIGURATION, Tconfigutation } from '../configuration';
+import { LoggerMiddleware } from '@commom/middlewares/logger.middleware';
 
 @Module({
-    imports: [ConfigModule.forRoot({ isGlobal: true , load: [()=>CONFIGURATION]})],
+    imports: [ConfigModule.forRoot({ isGlobal: true, load: [() => CONFIGURATION] })],
     controllers: [AppController],
     providers: [AppService],
 })
 export class AppModule {
     static CONFIGURATION: Tconfigutation = CONFIGURATION;
+
+    configure(consumer: MiddlewareConsumer) {
+        consumer.apply(LoggerMiddleware).forRoutes('*');
+    }
 }
